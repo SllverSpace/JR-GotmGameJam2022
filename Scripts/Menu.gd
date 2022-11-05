edabd6022a3005bcb8158d918bf41425ed9fac49
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-11-06 00:32:17 +1030

    e

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_Play_pressed():
-	Transition.switchScene("res://outside.tscn")
+	Transition.switchScene("res://Scenes/outside.tscn")
 
 func _process(delta):
 	$GhostMansion.position = -get_global_mouse_position()/32+Vector2(512, 320)
