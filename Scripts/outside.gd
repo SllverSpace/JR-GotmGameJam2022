a0c6a9c4c57980e3e910ca3ab462490018c70174
0versc0re <115696138+0versc0re@users.noreply.github.com>
2022-11-07 18:22:50 +1030

    .

--- Scripts/outside.gd
+++ Scripts/outside.gd
@@ -5,8 +5,6 @@ var flash = rand_range(5, 15)
 var flash2 = rand_range(0.3,1.8)
 var flash2_maybe = rand_range(1,2)
 
-
-
 func _ready():
 	$FLASH.visible = false
 	$Area2D2/Light.visible = true
@@ -32,12 +30,10 @@ func _process(delta):
 		lightNode.visible = $player.position.distance_to(lightNode.position) < 50
 		lightNode.enabled = $player.position.distance_to(lightNode.position) < 50
 		
-		yield(get_tree().create_timer(flash), "timeout")
-		$FLASH.visible = true
-		yield(get_tree().create_timer(0.2), "timeout")
-		$FLASH.visible = false
-		
-		
+	yield(get_tree().create_timer(flash), "timeout")
+	$FLASH.visible = true
+	yield(get_tree().create_timer(0.2), "timeout")
+	$FLASH.visible = false
 
 func _on_Area2D2_body_entered(body):
 	if body.name == "player":
