7de1a1dc2043df676200d65bf8479588be1c86a8
SilverSpace5 <112796153+SilverSpace5@users.noreply.github.com>
2022-11-08 18:42:23 +1030

    e

--- Scripts/outside.gd
+++ Scripts/outside.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 var timer = 0
-var flash = rand_range(5, 15)
-var flash2 = rand_range(0.3,1.8)
-var flash2_maybe = rand_range(1,2)
+#var flash = rand_range(5, 15)
+#var flash2 = rand_range(0.3,1.8)
+#var flash2_maybe = rand_range(1,2)
+var flashCooldown = rand_range(5, 15)
+var flashTimer = flashCooldown
+var flashTimer2 = flashCooldown+0.2
 
 func _ready():
+	flashCooldown = rand_range(5, 15)
 	$FLASH.visible = false
 	$Area2D2/Light.visible = true
-	$Tiles.modulate = Color(0.102, 0.102, 0.102)
+	$Tiles.modulate = Color(0.15, 0.15, 0.15)
 	UI.get_node("Time").visible = true
 	UI.timer = 0
 	UI.startTimer()
@@ -31,11 +35,28 @@ func _process(delta):
 			lightNode.energy = 10 - $player.position.distance_to(lightNode.position)/5
 		lightNode.visible = $player.position.distance_to(lightNode.position) < 50
 		lightNode.enabled = $player.position.distance_to(lightNode.position) < 50
-		
-	yield(get_tree().create_timer(flash), "timeout")
-	$FLASH.visible = true
-	yield(get_tree().create_timer(0.2), "timeout")
-	$FLASH.visible = false
+	
+	flashTimer -= delta
+	flashTimer2 -= delta
+	if flashTimer <= 0:
+		if flashTimer > -0.1:
+			$FLASH.visible = true
+		else:
+			$FLASH.visible = false
+			flashTimer = flashCooldown
+	if flashTimer2 <= 0:
+		if flashTimer2 > -0.1:
+			$FLASH.visible = true
+		else:
+			$FLASH.visible = false
+			flashTimer2 = flashCooldown+0.2
+			flashCooldown = rand_range(5, 15)
+	
+	
+#	yield(get_tree().create_timer(flash), "timeout")
+#	$FLASH.visible = true
+#	yield(get_tree().create_timer(0.2), "timeout")
+#	$FLASH.visible = false
 
 func _on_Area2D2_body_entered(body):
 	if body.name == "player":
